a54db1a5ddfe89c039d8c4bf87ec6021fa3e95c4
fundakartal <kartalfunda01@gmail.com>
2022-12-07 01:27:40 +0300

    Customize the default theme and add some styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,15 @@ module.exports = {
     './styles/**/*.{js,jsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-black': '#1A232E',
+        'secondary-white': '#c7c7c7',
+      },
+      transitionTimingFunction: {
+        'out-flex': 'cubic-bezier(0.05, 0.6, 0.4, 0.9)',
+      },
+    },
   },
   plugins: [],
 };
