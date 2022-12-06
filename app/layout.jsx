5099e66b82f27737077ff8a65e213a5255ee13ac
fundakartal <kartalfunda01@gmail.com>
2022-12-06 23:02:20 +0300

    Install tailwindCSS, eslint, framer-motion

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,14 @@
-import './globals.css'
+import '../styles/globals.css';
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      {/*
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    {/*
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
-}
+    <head />
+    <body>{children}</body>
+  </html>
+);
+
+export default RootLayout;
