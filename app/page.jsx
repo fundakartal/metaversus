32a3929eeac6d83edd8fdd4a913af6e030dff72d
fundakartal <kartalfunda01@gmail.com>
2022-12-15 20:10:29 +0300

    Add About section

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,14 @@
 import { Navbar } from '../components';
-import { Hero } from '../sections';
+import { About, Hero } from '../sections';
 
 const Page = () => (
   <div className="bg-primary-black overflow-hidden">
     <Navbar />
     <Hero />
+    <div className="relative">
+      <About />
+      <div className="gradient-03 z-0" />
+    </div>
   </div>
 );
 export default Page;
