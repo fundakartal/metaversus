b61b3e4bfcaeb3b3c1a06da18591dfcd609b6ec5
fundakartal <kartalfunda01@gmail.com>
2022-12-27 13:17:39 +0300

    Add World section

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 import { Navbar } from '../components';
-import { About, Explore, GetStarted, Hero, WhatsNew } from '../sections';
+import { About, Explore, GetStarted, Hero, WhatsNew, World } from '../sections';
 
 const Page = () => (
   <div className="bg-primary-black overflow-hidden">
@@ -15,6 +15,7 @@ const Page = () => (
       <div className="gradient-04 z-0" />
       <WhatsNew />
     </div>
+    <World />
   </div>
 );
 export default Page;
--- sections/index.jsx
+++ sections/index.jsx
@@ -3,5 +3,6 @@ import About from './About';
 import Explore from './Explore';
 import GetStarted from './GetStarted';
 import WhatsNew from './WhatsNew';
+import World from './World';
 
-export { Hero, About, Explore, GetStarted, WhatsNew };
+export { Hero, About, Explore, GetStarted, WhatsNew, World };
